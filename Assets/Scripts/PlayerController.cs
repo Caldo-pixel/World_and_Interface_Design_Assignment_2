870573afeefaa9c6bd1858b9b9d7ebb419f5fe78
Caldo-pixel <200602048@student.georgianc.on.ca>
2025-02-07 12:38:32 -0500

    started on movement and dodge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,29 @@
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    float speed = 100.0f;
+    //float dodgeForce = 100.0f;
+    float dodgeTimer = 0.5f;
+
+    bool isDodging;
+
+    Vector2 movement;
+    Vector2 direction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        sr.color = Color.white;
     }
 
     // Update is called once per frame
@@ -13,4 +31,52 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        if (isDodging)
+        {
+            return;
+        }
+
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+
+        direction = direction.normalized;
+        movement = direction * speed * Time.deltaTime;
+
+        rb.velocity = movement;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Dodge());
+        }
+    }
+
+    public IEnumerator Dodge()
+    {
+        isDodging = true;
+        sr.color = Color.green;
+        rb.velocity *= 2.0f;
+        yield return new WaitForSeconds(dodgeTimer);
+        isDodging = false;
+        sr.color = Color.white;
+    }
+
 }
