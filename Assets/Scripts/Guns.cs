8d6fc689c14056ba1a10b05c462e744d3e222e41
Caldo-pixel <200602048@student.georgianc.on.ca>
2025-02-14 14:23:52 -0500

    removed some unnecessary code

--- Assets/Scripts/Guns.cs
+++ Assets/Scripts/Guns.cs
@@ -75,31 +75,7 @@ public class Guns : MonoBehaviour
     void Update()
 
     {
-        //Vector3 direction = Vector3.zero;
-        //
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    direction += Vector3.up;
-        //}
-        //
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    direction += Vector3.down;
-        //}
-        //
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    direction += Vector3.left;
-        //}
-        //
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    direction += Vector3.right;
-        //}
-        //
-        //direction = direction.normalized;
-        //Vector3 movement = direction * moveSpeed * Time.deltaTime;
-        //transform.position += movement;
+        
 
         // Aiming with mouse 
         Vector3 mouse = Input.mousePosition;
